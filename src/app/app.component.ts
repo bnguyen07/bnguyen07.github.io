1edff51348d4878e8c303c23f66c0a3fc4d67212
Brian Nguyen <brian.nguyen@clearstructure.com>
2018-02-15 15:14:59 -0600

    Updated title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Brian Nguyen\'s Bio';
+  title = 'Jane\'s Papa\'s Bio';
 }
